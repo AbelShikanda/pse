46c46c6977dd5ba5918d1f32e832a910b7ab0291
abel <abelshikanda3@gmail.com>
2025-03-07 18:47:30 +0300

    chore: added categories to catolog

--- resources/views/pages/catalog.blade.php
+++ resources/views/pages/catalog.blade.php
@@ -31,7 +31,7 @@
                 <div class="row align-items-center justify-content-center w-100 col-lg-8 text-center">
 
                     <!-- Dropdown (Select Category) -->
-                    <!-- <div class="col-md-4 pb-2">
+                    <div class="col-md-4 pb-2">
                         <div class="dropdown w-100">
                             <button class="btn btn-outline-secondary dropdown-toggle py-2 w-100 home_btn" type="button"
                                 id="shopDropdown" data-bs-toggle="dropdown" aria-expanded="false">
@@ -46,14 +46,13 @@
                                 @endforeach
                             </ul>
                         </div>
-                    </div> -->
+                    </div>
 
                     <!-- Search Bar & Like Button -->
                     <div class="col-md-6">
                         <div class="input-group justify-content-center flex-wrap">
                             <input type="text" class="form-control py-2 w-50" placeholder="Search..."
                                 aria-label="Search" id="search-transparent">
-
                             <button class="btn btn-outline-secondary px-3" type="button" id="searchButton">
                                 <i class="bi bi-search"></i>
                             </button>
